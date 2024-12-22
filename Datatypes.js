de226ee1503286f1d01d615ea0869c10750222f7
suraj830 <123144316+suraj830@users.noreply.github.com>
2024-12-22 08:43:02 +0000

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+// aleart(3+3)  //we are using node.js not web browser
+
+console.log(3 + 3); // code readability should be high
+console.log("suraj");
+
+let name="suraj"
+let age="25"
+let isloogedIn=false
+
+
+//number=>2 to power 53
+// bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>umique
+//object
+
+console.log(typeof "suraj" );
+console.log(typeof null );
+console.log(typeof undefined );
