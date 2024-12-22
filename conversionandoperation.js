b03bbbcc72225b8a05b718c97442eb4078443c32
suraj830 <123144316+suraj830@users.noreply.github.com>
2024-12-22 10:03:50 +0000

    conversion

---
+++ conversionandoperation.js
@@ -0,0 +1,27 @@
+let score=33
+let score2="33abc"
+let score3=null
+
+console.log(typeof score);
+console.log(typeof(score));
+console.log(typeof score2);
+console.log(typeof(score2));
+
+let valueInNumber=Number(score3)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
