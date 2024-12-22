cf342326a96cf4f8bf45953e20f668717f07642c
suraj830 <123144316+suraj830@users.noreply.github.com>
2024-12-22 13:51:59 +0000

    stackandheap.js

---
+++ stackandheap.js
@@ -0,0 +1,22 @@
+// Stack memory(primitive), heap memory(Non-primitive)
+
+let myYoutubename="surajprakashdotcom"
+
+let anotherName=myYoutubename
+anotherName="chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+let userOne={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo=userOne
+userTwo.email="suraj@gamil.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
