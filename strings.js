e3b7dbf5452449954fe02b8292a3495a056b3438
suraj830 <123144316+suraj830@users.noreply.github.com>
2024-12-23 08:07:43 +0000

    strings

---
+++ strings.js
@@ -0,0 +1,25 @@
+const name="Hitesh"
+const repoCount=50
+console.log(name+repoCount+" value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName=new String('Hitesh')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('r'));
+
+const newString=gameName.substring(-8,4)
+console.log(newString);
+
+const anotherString=gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne=" hitesh   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
