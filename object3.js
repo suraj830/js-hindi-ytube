2c5863c10a58b7c1315587d6b0e4d7fba6cdafeb
suraj830 <123144316+suraj830@users.noreply.github.com>
2024-12-24 06:43:03 +0000

    jsontalk

---
+++ object3.js
@@ -0,0 +1,23 @@
+const course ={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"suraj"
+}
+// course.courseInstructor
+const  {courseInstructor: instructor}=course
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//    "name":"suraj",
+//     "courcename":"js in hindi",
+//     "price" : "free"
+
+// }
+
+
+[
+    {},
+    {},
+    {},
+]
